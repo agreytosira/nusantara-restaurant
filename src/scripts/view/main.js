af33e5ac979ebb753ca6becdae496cda67775734
Agrey Tosira <agreytosira@gmail.com>
2023-09-29 19:51:53 +0700

    refactor(data): remove fetch and using standart import for get data

--- src/scripts/view/main.js
+++ src/scripts/view/main.js
@@ -1,46 +1,30 @@
+import data from '../../public/data/DATA.json'
+
 const main = () => {
-  function getRestaurants() {
-    return fetch('/data/DATA.json')
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Gagal mengambil data restoran')
-        }
-        return response.json()
-      })
-      .then((responseJson) => {
-        return responseJson.restaurants
-      })
-      .catch((error) => {
-        alert.log(error)
-      })
+  function renderRestaurants(restaurants) {
+    const restaurantContent = document.querySelector('.restaurant__content')
+    restaurantContent.innerHTML = ''
+
+    restaurants.forEach((restaurant) => {
+      restaurantContent.innerHTML += `
+      <div class="restaurant-card" tabindex="0">
+        <div class="restaurant-card__thumbnail">
+          <img src="${restaurant.pictureId}" alt="Foto Restoran ${restaurant.name}" />
+          <span class="restaurant-card__city">${restaurant.city}</span>
+        </div>
+        <div class="restaurant-card__body">
+          <div class="restaurant-card__rating"><i class="ri-star-fill"></i> ${restaurant.rating}</div>
+          <div class="restaurant-card__name"><h5>${restaurant.name}</h5></div>
+          <div class="restaurant-card__desc">
+            <p>${restaurant.description}</p>
+          </div>
+        </div>
+      </div>
+    `
+    })
   }
 
-  getRestaurants()
-    .then((restaurants) => {
-      const restaurantContent = document.querySelector('.restaurant__content')
-      restaurantContent.innerHTML = ''
-
-      restaurants.forEach((restaurant) => {
-        restaurantContent.innerHTML += `
-          <div class="restaurant-card">
-            <div class="restaurant-card__thumbnail">
-              <img src="${restaurant.pictureId}" alt="Foto Restoran ${restaurant.name}" />
-              <span class="restaurant-card__city">${restaurant.city}</span>
-            </div>
-            <div class="restaurant-card__body">
-              <div class="restaurant-card__rating"><i class="ri-star-fill"></i> ${restaurant.rating}</div>
-              <div class="restaurant-card__name"><h5>${restaurant.name}</h5></div>
-              <div class="restaurant-card__desc">
-                <p>${restaurant.description}</p>
-              </div>
-            </div>
-          </div>
-        `
-      })
-    })
-    .catch((error) => {
-      console.log(error)
-    })
+  renderRestaurants(data.restaurants)
 
   const btnToggle = document.querySelector('.btn-toggle')
   const navbarList = document.querySelector('.navbar__list')

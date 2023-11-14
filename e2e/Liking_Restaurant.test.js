6629d248d1a95eaf5a8549213c84662b558a2a79
Agrey Tosira <agreytosira@gmail.com>
2023-11-14 23:34:07 +0700

    test: add e2e test for liking one restaurant

--- e2e/Liking_Restaurant.test.js
+++ e2e/Liking_Restaurant.test.js
@@ -6,7 +6,31 @@ Before(({ I }) => {
   I.amOnPage('/#/favorite')
 })
 
-Scenario('showing empty liked food', ({ I }) => {
+Scenario('showing empty favorited restaurant', ({ I }) => {
   I.seeElement('h4')
   I.see('Currently no restaurant added to favorites', 'h4')
+})
+
+Scenario('liking one restaurant', async ({ I }) => {
+  I.see('Currently no restaurant added to favorites', 'h4')
+
+  I.amOnPage('/')
+
+  I.seeElement('.restaurant-card')
+  const restaurantCard = locate('.restaurant-card').first()
+  const restaurantCardName = await I.grabTextFrom('h5')
+  I.click(restaurantCard)
+
+  I.seeElement('#likeButton')
+  I.click('#likeButton')
+
+  I.wait(1)
+
+  I.amOnPage('/#/favorite')
+  I.seeElement('.restaurant-card')
+  const likedRestaurantName = await I.grabTextFrom('h5')
+
+  assert.strictEqual(restaurantCardName, likedRestaurantName)
+
+  pause()
 })

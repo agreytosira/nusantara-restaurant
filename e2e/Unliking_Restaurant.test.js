c28f6a94a2fa198ee72c50019d49a10dcb2a71cb
Agrey Tosira <agreytosira@gmail.com>
2023-11-15 00:12:51 +0700

    test: add e2e test for unliking one restaurant

--- e2e/Unliking_Restaurant.test.js
+++ e2e/Unliking_Restaurant.test.js
@@ -1,31 +1,33 @@
 const assert = require('assert')
 
-Feature('Liking Restaurant')
+Feature('Unliking Restaurant')
 
 Before(({ I }) => {
   I.amOnPage('/#/favorite')
 })
 
-Scenario('showing empty favorited restaurant', ({ I }) => {
+Scenario('menghilangkan restaurant dari daftar favorit', async ({ I }) => {
   I.see('Currently no restaurant added to favorites', 'h4')
-})
 
-Scenario('liking one restaurant', async ({ I }) => {
   I.amOnPage('/')
-
   I.seeElement('.restaurant-card')
   const restaurantCard = locate('.restaurant-card').first()
   const restaurantCardName = await I.grabTextFrom('h5')
   I.click(restaurantCard)
-
   I.seeElement('#likeButton')
   I.click('#likeButton')
-
   I.wait(1)
 
   I.amOnPage('/#/favorite')
   I.seeElement('.restaurant-card')
   const likedRestaurantName = await I.grabTextFrom('h5')
-
   assert.strictEqual(restaurantCardName, likedRestaurantName)
+
+  I.click('.restaurant-card')
+  I.seeElement('#likeButton')
+  I.click('#likeButton')
+  I.wait(1)
+
+  I.amOnPage('/#/favorite')
+  I.see('Currently no restaurant added to favorites', 'h4')
 })

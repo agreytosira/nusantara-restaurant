79a7850822683ffb74e44ae5dcaf9e644d98d819
Agrey Tosira <agreytosira@gmail.com>
2023-11-15 11:40:52 +0700

    test: add e2e test for add 3 restaurant to favorite

--- e2e/Liking_Restaurant.test.js
+++ e2e/Liking_Restaurant.test.js
@@ -29,3 +29,29 @@ Scenario('liking one restaurant', async ({ I }) => {
 
   assert.strictEqual(restaurantCardName, likedRestaurantName)
 })
+
+Scenario('menambahkan tiga restaurant ke daftar favorit', async ({ I }) => {
+  for (let i = 1; i < 4; i++) {
+    // Mengakses halaman utama
+    I.amOnPage('/')
+
+    // Mengklik restaurant-card dengan indeks i
+    I.seeElement('.restaurant-card')
+    const restaurantCard = locate('.restaurant-card').at(i)
+    const restaurantCardName = await I.grabTextFrom(locate('.restaurant-card h5').at(i))
+    I.click(restaurantCard)
+
+    // Menunggu dan mengklik tombol 'like'
+    I.seeElement('#likeButton')
+    I.click('#likeButton')
+    I.wait(1)
+
+    // Pindah ke halaman favorit
+    I.amOnPage('/#/favorite')
+    I.seeElement('.restaurant-card')
+
+    // Memeriksa apakah restaurant-card ditambahkan ke daftar favorit dengan benar
+    const likedRestaurantName = await I.grabTextFrom(locate('.restaurant-card h5').at(i))
+    assert.strictEqual(restaurantCardName, likedRestaurantName)
+  }
+})

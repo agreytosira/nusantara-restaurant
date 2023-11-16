7d862770a6af9186606ebf1ee252074f51550274
Agrey Tosira <agreytosira@gmail.com>
2023-11-16 22:59:11 +0700

    fix: reconfigure bundle analyzer

--- webpack.prod.js
+++ webpack.prod.js
@@ -60,5 +60,12 @@ module.exports = merge(common, {
       }
     ]
   },
-  plugins: [new FaviconsWebpackPlugin('./src/public/favicon.png'), new CleanWebpackPlugin(), new BundleAnalyzerPlugin()]
+  plugins: [
+    new FaviconsWebpackPlugin('./src/public/favicon.png'),
+    new CleanWebpackPlugin(),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      openAnalyzer: false
+    })
+  ]
 })

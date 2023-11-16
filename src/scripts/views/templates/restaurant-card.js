6aaac8cf3ff05a31fa2dcf7371498a6ab5e6e5c0
Agrey Tosira <agreytosira@gmail.com>
2023-11-16 10:56:20 +0700

    style: update restaurant city label color and add pin location icon

--- src/scripts/views/templates/restaurant-card.js
+++ src/scripts/views/templates/restaurant-card.js
@@ -4,7 +4,7 @@ const restaurantCard = (restaurant) => `
     <a href="#/restaurant/${restaurant.id}" class="restaurant-card">
         <div class="restaurant-card__thumbnail">
         <img class="lazyload" src="${CONFIG.BASE_IMAGE_URL_SMALL + restaurant.pictureId}" alt="Foto Restaurantran ${restaurant.name}" />
-          <span class="restaurant-card__city">${restaurant.city}</span>
+          <span class="restaurant-card__city"><i class="ri-map-pin-fill"></i>${restaurant.city}</span>
         </div>
         <div class="restaurant-card__body">
           <div class="restaurant-card__rating"><i class="ri-star-fill"></i> ${restaurant.rating}</div>

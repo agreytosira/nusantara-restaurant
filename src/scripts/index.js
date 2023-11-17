5d86021b2b57e4cfa68a4830acb92a5eff8b38b0
Agrey Tosira <agreytosira@gmail.com>
2023-11-17 14:41:32 +0700

    optimize: swap google font by using local font, fix remixicon

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,6 @@
 import 'regenerator-runtime'
 import '../styles/main.scss'
+import '../public/fonts/remixicon.css'
 import 'lazysizes'
 import 'lazysizes/plugins/parent-fit/ls.parent-fit'
 import App from './views/App'
